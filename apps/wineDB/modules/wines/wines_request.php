70a908e32e214a382cee7f237ca0c7f146a25450
Dominique Dietiker <dominique.dietiker@momoland.ch>
2015-09-10 20:16:58 +0200

    Appellation
    Additional field

--- apps/wineDB/modules/wines/wines_request.php
+++ apps/wineDB/modules/wines/wines_request.php
@@ -5,8 +5,8 @@
 	class WineRequest extends Request {
 		
 		private $selectQuery = "SELECT * FROM wines JOIN (wine_producers, wine_types) ON (wines.producer_id=wine_producers.producer_id AND wines.winetype_id=wine_types.winetype_id)";
-		private $updateQuery = "UPDATE wines SET wine_name=:wineName, producer_id=:prodId, wine_alcohol=:wineAlcohol, wine_grapes=:wineGrapes, winetype_id=:winetypeId, wine_notes=:wineNotes WHERE wine_id=:wineId";
-		private $insertQuery = "INSERT INTO wines (wine_name, producer_id, winetype_id, wine_alcohol, wine_grapes, wine_notes) VALUES (:wineName, :producerId, :winetypeId, :wineAlcohol, :wineGrapes, :wineNotes)";
+		private $updateQuery = "UPDATE wines SET wine_name=:wineName, wine_appellation=:appellation, producer_id=:prodId, wine_alcohol=:wineAlcohol, wine_grapes=:wineGrapes, winetype_id=:winetypeId, wine_notes=:wineNotes WHERE wine_id=:wineId";
+		private $insertQuery = "INSERT INTO wines (wine_name, wine_appellation, producer_id, winetype_id, wine_alcohol, wine_grapes, wine_notes) VALUES (:wineName, :appellation, :producerId, :winetypeId, :wineAlcohol, :wineGrapes, :wineNotes)";
 		
 		public function getWines() {	
 			$stmt = $this->connection->prepare($this->selectQuery);		
@@ -32,6 +32,7 @@
 		public function updateWineWithId($data) {
 			$stmt = $this->connection->prepare($this->updateQuery);
 			$stmt->bindValue(":wineName", $data["wine_name"]);	
+			$stmt->bindValue(":appellation", $data["wine_appellation"]);	
 			$stmt->bindValue(":prodId", $data["producer_id"]);	
 			$stmt->bindValue(":wineAlcohol", $data["wine_alcohol"]);	
 			$stmt->bindValue(":wineGrapes", $data["wine_grapes"]);	
@@ -49,6 +50,7 @@
 		public function insertWine($data) {
 			$stmt = $this->connection->prepare($this->insertQuery);
 			$stmt->bindValue(":wineName", $data["wine_name"]);	
+			$stmt->bindValue(":appellation", $data["wine_appellation"]);	
 			$stmt->bindValue(":producerId", $data["producer_id"]);	
 			$stmt->bindValue(":winetypeId", $data["winetype_id"]);	
 			$stmt->bindValue(":wineAlcohol", $data["wine_alcohol"]);	
