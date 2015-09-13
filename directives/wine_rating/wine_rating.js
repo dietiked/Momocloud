e524427f3aa807ee23e4177bb1eb577d87812de2
Dominique Dietiker <dominique.dietiker@momoland.ch>
2015-09-13 20:50:24 +0200

    Small changes to UI

--- directives/wine_rating/wine_rating.js
+++ directives/wine_rating/wine_rating.js
@@ -1,14 +1,14 @@
 function wineRating () {
 	
 	return {
-		restrict: 'AE',
+		restrict: 'E',
 		templateUrl: '../../directives/wine_rating/wine_rating.html',
 		replace: false,
 		scope: {
 			'wine_id': '@wine',
 			'vintage_id': '@vintage',
 			'vintage_year': '@year',
-			'vintage_rating': '@rating',
+			'vintage_rating': '@vintagerating',
 		},
 		link: function($scope, $element, $attrs) {
 			
