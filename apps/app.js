7bff85d0d74e2e0f11dda7ee389a611aff1b95f2
Dominique Dietiker <dominique.dietiker@momoland.ch>
2016-01-16 18:47:05 +0100

    Change space to tabs in app.js

--- apps/app.js
+++ apps/app.js
@@ -26,49 +26,49 @@ momocloudServices.directive('recipiesRecipeForm', recipiesRecipeForm);
 var momocloud = angular.module('momocloud', ['ngRoute', 'momocloudControllers', 'momocloudServices', 'momocloudDirectives', 'angular.filter', 'ui.bootstrap', 'ngTagsInput'], 
 	
 	function($httpProvider) {
-	  // Use x-www-form-urlencoded Content-Type
-	  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+		// Use x-www-form-urlencoded Content-Type
+		$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 	
-	  /**
-	   * The workhorse; converts an object to x-www-form-urlencoded serialization.
-	   * @param {Object} obj
-	   * @return {String}
-	   */ 
-	  var param = function(obj) {
-	    var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
-	      
-	    for(name in obj) {
-	      value = obj[name];
-	        
-	      if(value instanceof Array) {
-	        for(i=0; i<value.length; ++i) {
-	          subValue = value[i];
-	          fullSubName = name + '[' + i + ']';
-	          innerObj = {};
-	          innerObj[fullSubName] = subValue;
-	          query += param(innerObj) + '&';
-	        }
-	      }
-	      else if(value instanceof Object) {
-	        for(subName in value) {
-	          subValue = value[subName];
-	          fullSubName = name + '[' + subName + ']';
-	          innerObj = {};
-	          innerObj[fullSubName] = subValue;
-	          query += param(innerObj) + '&';
-	        }
-	      }
-	      else if(value !== undefined && value !== null)
-	        query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
-	    }
-	      
-	    return query.length ? query.substr(0, query.length - 1) : query;
-	  };
-	
-	  // Override $http service's default transformRequest
-	  $httpProvider.defaults.transformRequest = [function(data) {
-	    return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
-	  }];
+		/**
+		* The workhorse; converts an object to x-www-form-urlencoded serialization.
+		* @param {Object} obj
+		* @return {String}
+		*/
+		var param = function(obj) {
+			var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+
+			for(name in obj) {
+				value = obj[name];
+
+				if(value instanceof Array) {
+					for(i=0; i<value.length; ++i) {
+						subValue = value[i];
+						fullSubName = name + '[' + i + ']';
+						innerObj = {};
+						innerObj[fullSubName] = subValue;
+						query += param(innerObj) + '&';
+					}
+				}
+				else if(value instanceof Object) {
+					for(subName in value) {
+						subValue = value[subName];
+						fullSubName = name + '[' + subName + ']';
+						innerObj = {};
+						innerObj[fullSubName] = subValue;
+						query += param(innerObj) + '&';
+					}
+				}
+				else if(value !== undefined && value !== null)
+					query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+				}
+
+				return query.length ? query.substr(0, query.length - 1) : query;
+			};
+		
+			// Override $http service's default transformRequest
+			$httpProvider.defaults.transformRequest = [function(data) {
+				return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+			}];
 });
 
 momocloud.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
@@ -205,15 +205,15 @@ momocloud.run(function($rootScope, $location, AuthService) {
 	// enumerate routes that don't need authentication
 	var routesThatDontRequireAuth = ['/login'];
 
-	// check if current location matches route  
+	// check if current location matches route	 
 	var routeClean = function (route) {
 		return _.find(routesThatDontRequireAuth,
-    		function (noAuthRoute) {
+			function (noAuthRoute) {
 				if (noAuthRoute.split('/')[1] === route.split('/')[1]) {
-		    		return true;				
+					return true;				
 				}
-	    	}
-	    );
+			}
+		);
 	};
 
 	$rootScope.$on('$routeChangeStart', function (event, next, current) {
