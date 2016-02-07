87f574cd5b8643cd1c7196577881ffcee09cabf6
Dominique Dietiker <dominique.dietiker@momoland.ch>
2016-02-07 09:32:35 +0100

    UrlService notification for Library
    - Add in every controller of the Library app UrlService notification
    - Remove title in Library app views

--- src/apps/start/start_controller.js
+++ src/apps/start/start_controller.js
@@ -1,10 +1,16 @@
-momocloudStart.controller('StartController', 
+momocloudStart.controller('StartController',
 ['$scope', 'NotificationCenter', 'DependenciesChecker', 'UrlService', 'AuthService',
 function($scope, NotificationCenter, DependenciesChecker, UrlService, AuthService) {
 
-	console.log('StartController');	
+	console.log('StartController');
+
+	NotificationCenter.postNotification(UrlService.notifications.REDIRECT_SUCCESS, [{
+		title: 'Start',
+		application: 'Momocloud'
+	}]);
+
 	AuthService.increaseExpiration();
-	
+
 	$scope.go = function(url) {
 		UrlService.go(url);
 	}
